95b09f74739969e8e84b1a765fcb0b7d7e1ffb1b
Mauricio <mauricio@yahoo.com>
2023-06-21 15:28:44 -0600

    clonning

--- jenkinsfile.ci.groovy
+++ jenkinsfile.ci.groovy
@@ -1,20 +1,13 @@
 // Define secret variables
-def MY_PASSWORD = "YWVyY3dxZWY"
-def MY_SECRET = "ZGZoeWt5OGt"
+def MY_PASSWORD = 'YWVyY3dxZWY'
+def MY_SECRET = 'ZGZoeWt5OGt'
 
 // Mask secret variables and try to print
 pipeline {
   agent any
   stages {
-    stage ("Print variable") {
-      steps {
-        wrap([$class: "MaskPasswordsBuildWrapper",
-              varPasswordPairs: [[password: MY_PASSWORD],
-                                 [password: MY_SECRET]]]) {
-          echo "Password: ${MY_PASSWORD}"
-          echo "Secret: ${MY_SECRET}"
-        }
-      }
+    stage('checkout') {
+      git credentialsId: 'github-credentials', url: 'https://github.com/ciokma/hello-java-spring-boot.git', branch: 'main'
     }
   }
 }
